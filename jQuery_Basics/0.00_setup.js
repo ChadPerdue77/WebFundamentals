80f8e7c5654368f228ca726461b0f9747b606c3d
Chad Perdue <theperduer@gmail.com>
2017-06-27 09:13:52 -0400

    jQuery

---
+++ jQuery_Basics/0.00_setup.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$("#hide").click(function(){
+
+		$("#image").hide();
+
+	$("#show").click(function(){
+
+		$("#image").show();
+
+	$("#explode").click(function(){
+
+		$("#image").toggle("explode", 2000);
+
+
+	// $("#fade_in").click(function)(){
+
+	// 	$("#image").fade
+
+	// $("#fade_out").click(function)(){
+
+	// 	$("#image").fade
+
+
+	
+					});
+				});
+			});
+		});
+// 	});
+// });
